aaa2ff736dc6e6366af49ac2da38b2e542e7e14d
BlakeBrooker <116875628+BlakeBrooker@users.noreply.github.com>
2022-11-08 15:57:20 +0000

    Add files via upload

--- js/sketch.js
+++ js/sketch.js
@@ -6,6 +6,35 @@ function setup() {
 function draw() {
     background(220);
 
+
+    fill(0,0,0);
+    circle(400, 400, 600);
+
+    fill(255,255,255);
+    circle(400, 400, 500);
+
+    fill(0,0,0);
+    circle(400, 400, 400);
+
     fill(255, 255, 255);
-    ellipse(400, 400, 200);
+    circle(400, 400, 300);
+
+    fill(0,0,0);
+    circle(400, 400, 200);
+
+    fill(255, 255, 255);
+    circle(400, 400, 100);
+
+    fill(0,0,0);
+    circle(400, 400, 50);
+
+    fill(255, 255, 255);
+    circle(400, 400, 25);
+
+    fill(0,0,0);
+    line(25, 100, 780, 700);
+    
+    fill(0,0,0);
+    line(700, 100, 100, 700);
+  
 }
